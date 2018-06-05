bc325dafa5e6228e6143005d3cbaac538371c862
Kai Yang <kfstorm@dingtalk.com>
2018-06-05 18:18:30 +0800

    Introduce Spring Data REST

--- src/main/java/com/mycompany/boston/pipeline/Application.java
+++ src/main/java/com/mycompany/boston/pipeline/Application.java
@@ -1,11 +1,12 @@
-package video;
+package com.mycompany.boston.pipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
